1e58b7b8415c4e94b299c03227b599c11d6c2cd9
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-30 20:40:18 -0300

    Apply listeners and logic do draw the path on the board

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import { renderUI, setFavicon } from './scripts/UI';
+import { applyListeners } from './scripts/DOMManipulation';
 
 renderUI();
 setFavicon();
+applyListeners();
